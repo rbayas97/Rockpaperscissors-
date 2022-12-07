fa498763ef683115d5fe1e11d0af44ad047e6652
Raul <76450597+rbayas97@users.noreply.github.com>
2022-12-07 16:28:22 -0500

    Add function getComputerChoice to app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+function getComputerChoice(){
+    let compchoices = ['Rock', 'Paper', 'Scissors'];
+    let randomchoices = compchoices[Math.floor(Math.random() * compchoices.length)];
+    return randomchoices;
+};
+
+console.log(getComputerChoice());
