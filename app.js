2719340f2f023806cdf1f6c64755a23bbc725abd
Raul <76450597+rbayas97@users.noreply.github.com>
2022-12-26 10:38:34 -0500

    Change variable scope for buttons

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
-let playerButton; 
-const buttons = document.querySelectorAll('button');
+
 const body = document.querySelector('body');
-const message = document.createElement('div')
+const outcome = document.createElement('div');
 const content = document.createElement('div');
+
 
 function getComputerChoice(){
     let compchoices = ['Rock', 'Paper', 'Scissors'];
@@ -14,36 +14,44 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        message.textContent = 'Draw! Nobody gets a point';
-        body.appendChild(message);
+        outcome.textContent = 'Draw! Nobody gets a point';
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
     }  else if (playerSelection === 'Rock' && computerSelection === 'Paper'){
         computerScore += 1;
-        message.textContent =  "You lose! Paper beats Rock";
-        body.appendChild(message);
+        outcome.textContent =  "You lose! Paper beats Rock";
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
     }  else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
         playerScore += 1;
-        message.textContent = "You win! Rock beats Scissors"; 
-        body.appendChild(message);
+        outcome.textContent = "You win! Rock beats Scissors";
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
     }  else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
         playerScore += 1;
-        message.textContent = "You win! Paper beats Rock"; 
-        body.appendChild(message);
+        outcome.textContent = "You win! Paper beats Rock";
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
     }  else if (playerSelection === 'Paper' && computerSelection === "Scissors"){
         computerScore += 1;
-        message.textContent = "You lose! Scissors beats Paper"; 
-        body.appendChild(message);
+        outcome.textContent = "You lose! Scissors beats Paper";
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
     }  else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
         computerScore += 1;
-        message.textContent =  "You lose! Rock beats Scissors"; 
-        body.appendChild(message);
+        outcome.textContent =  "You lose! Rock beats Scissors";
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
     }  else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
         playerScore += 1;
-        message.textContent = "You win! Scissors beats Paper";
-        body.appendChild(message);
+        outcome.textContent = "You win! Scissors beats Paper";
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
     }  else {
-        message.textContent = "We can't play the game";
-        body.appendChild(message);
-    } 
+        outcome.textContent = "We can't play the game";
+        body.appendChild(outcome);
+        body.insertBefore(outcome,content);
+    }
 
 }
 
@@ -55,27 +63,28 @@ function scoreTable() {
 
 function updateScore() {
     content.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
+
 }
 
 function winningUser() {
     if (playerScore == 5){
         playerScore = 0;
         computerScore = 0;
-        alert("You won the Game! Press OK to play Again")
+        alert("You won the Game! Make your choice to play Again");
     } else if (computerScore == 5){
         playerScore = 0;
         computerScore = 0;
-        alert("You lost the game! Press OK to play Again")
-    } updateScore();
+        alert("You lost the game! Make your choice to play Again");
+    }
 }
 
 function game() {
+    const buttons = document.querySelectorAll('button');
     scoreTable();
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            playerButton = button.textContent;
-            playerSelection = playerButton;
-            computerSelection = getComputerChoice();
+            let playerSelection = button.textContent;
+            let computerSelection = getComputerChoice();
             playRound(playerSelection, computerSelection);
             updateScore();
             winningUser();
